46102cdd1c1b2304bb0ffb5c0eb023969bd75871
jasontcrabtree <jasontcrabtree@gmail.com>
2020-11-13 23:58:52 +1300

    subgrid and data loaded into pizzas file templated out

--- starter-files/gatsby/src/components/PizzaList.js
+++ starter-files/gatsby/src/components/PizzaList.js
@@ -3,37 +3,61 @@ import React from 'react';
 import styled from 'styled-components';
 import Img from 'gatsby-image';
 
-const StyledPizzaList = styled.ul`
+const PizzaGridStyles = styled.ul`
+  list-style-type: none;
+  margin: 0px;
+  padding: 0px;
+  display: grid;
+  gap: 4rem;
+  grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+
+  grid-auto-rows: auto auto 240px;
+`;
+
+const PizzaStyles = styled.li`
   list-style-type: none;
   margin: 0px;
   padding: 0px;
 
-  li {
-    margin-top: 2rem;
+  display: grid;
+
+  /* Polyfill */
+  @supports not (grid-template-rows: subgrid) {
+    --rows: auto auto 280px;
+  }
+  /* Subgrid instructs browswer to take the row sizing from the parent PizzaGridStyles sizing */
+  /* Subgrid Not supported in chrome 13 Nov 2020 */
+  grid-template-rows: var(--rows, subgrid);
+
+  grid-row: span 3;
+  grid-gap: 1rem;
+
+  h2,
+  p {
+    margin: 0px;
   }
 `;
 
 function SinglePizza({ pizza }) {
   return (
-    <li>
+    <PizzaStyles>
       <Link to={`/pizza/${pizza.slug.current}`}>
         <h2>
           <span className="mark">{pizza.name}</span>
         </h2>
-        <p>{pizza.toppings.map((topping) => topping.name).join(', ')}</p>
-        <Img fluid={pizza.image.asset.fluid} alt={pizza.name} />
       </Link>
-    </li>
+      <p>{pizza.toppings.map((topping) => topping.name).join(', ')}</p>
+      <Img fluid={pizza.image.asset.fluid} alt={pizza.name} />
+    </PizzaStyles>
   );
 }
 
 export default function PizzaList({ pizzas }) {
-  console.log(pizzas);
   return (
-    <StyledPizzaList>
+    <PizzaGridStyles>
       {pizzas.map((pizza) => (
         <SinglePizza key={pizza.id} pizza={pizza} />
       ))}
-    </StyledPizzaList>
+    </PizzaGridStyles>
   );
 }
